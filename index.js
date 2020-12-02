cb3a2b6df4ea85fdc804c6c8333019c9e5fa18b1
aakash thapa <aakash_conky@hotmail.com>
2020-12-02 05:30:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `"Let's talk about" + ${topic}`;
+var open = "Let's talk about";
+
+var conversation = `${open} + ${topic}`;
